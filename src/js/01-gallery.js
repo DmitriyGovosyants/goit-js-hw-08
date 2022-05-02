8ee2d9dfab16ab1bd6254f79cc8bb2c5382e2929
Dmitriy Govosyants <91630215+DmitriyGovosyants@users.noreply.github.com>
2022-05-02 10:10:26 +0300

    features/gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const galleryEl = document.querySelector('.gallery');
+
+function makeGalleryMarkup() {
+    const markup = galleryItems
+    .map(({preview, original, description}) => 
+        `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}">
+        </a>`
+    )
+    .join('');
+
+    galleryEl.innerHTML = markup;
+};
+
+makeGalleryMarkup();
+const lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250, disableScroll: false});
